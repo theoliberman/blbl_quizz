5bcc1d1cddd1ab0521382c8f04c2d243c6e7cf05
Théo Liberman <theo@ct-square.com>
2020-06-02 16:16:08 +0200

    fix sameSite cookie attribute

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module BlblQuiz
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.session_store :cookie_store, key: '...', same_site: :lax
   end
 end
